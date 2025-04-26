b2ed26922ec8b07c4a55f74a5d5fb794d957e773
caneert <abdullahtemiz@hacettepe.edu.tr>
2025-04-26 14:20:41 +0300

    b

---
+++ NotAGame/Assets/startbutton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class StartButton : MonoBehaviour
+{
+    public List<Button> allButtons = new List<Button>();
+
+    void Start()
+    {
+        if (allButtons.Count == 0)
+        {
+            Debug.LogWarning("Hiç buton atanmadı!");
+            return;
+        }
+
+        // Bütün butonlara aynı geçiş eventi ekleniyor
+        foreach (Button button in allButtons)
+        {
+            button.onClick.AddListener(GoToFightScene);
+        }
+    }
+
+    void GoToFightScene()
+    {
+        SceneManager.LoadScene("fight scene"); // Hangi butona basarsan sahne değişir
+    }
+}
